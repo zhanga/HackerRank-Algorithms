6f142eaaffc708142a9bf728cb0f67ced7dd0fbc
Anthony H. Zhang <anthony.huazhang@gmail.com>
2017-11-29 13:13:16 -0500

    Staircase

---
+++ Staircase/Staircase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int differenceOfN = 0;
+        for (int i = 0; i < n; i++)
+        {
+            differenceOfN = 1;
+            while (differenceOfN < n - i)
+            {
+                Console.Write(" ");
+                differenceOfN++;
+            }
+            while (differenceOfN != n)
+            {
+                Console.Write("#");
+                differenceOfN++;
+            }
+            Console.WriteLine("#");
+            differenceOfN = 0;
+        }
+        Console.ReadKey();
+    }
+}
