509b8697f9ebaf764a33eeedf69b9f06b17f10b1
Anthony H. Zhang <anthony.huazhang@gmail.com>
2017-12-01 17:51:15 -0500

    added Time Conversion

---
+++ Time Conversion/Time Conversion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Solution
+{
+    static string timeConversion(string s)
+    {
+        string meridiemInfo = s.Substring(8, 2);
+        int hours = Convert.ToInt32(s.Substring(0, 2));
+        if (meridiemInfo == "PM" && hours != 12)
+        {
+            hours += 12;
+            s = hours + (s.Substring(2, 6));
+        }
+        else if (meridiemInfo == "AM" && hours == 12)
+        {
+            s = "00" + (s.Substring(2, 6));
+        }
+        else
+        {
+            s = s.Substring(0, 8);
+        }
+        return (s);
+    }
+
+    static void Main(String[] args)
+    {
+        string s = Console.ReadLine();
+        string result = timeConversion(s);
+        Console.WriteLine(result);
+    }
+}
---
+++ TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Solution
+{
+
+    static string timeConversion(string s)
+    {
+        int timeValue = Convert.ToInt32(s.IndexOf(":"));
+        if (s.Contains("PM"))
+        {
+
+        }
+
+        else if (s.Contains("AM"))
+        {
+
+        }
+        Console.WriteLine(timeValue);
+        Console.ReadKey();
+        return s;
+    }
+
+    static void Main(String[] args)
+    {
+        string s = Console.ReadLine();
+        string result = timeConversion(s);
+        Console.WriteLine(result);
+    }
+}
