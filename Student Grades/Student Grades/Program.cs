a525012fac361f1b68d8a1a49b5c4e96fc252bb3
Anthony H. Zhang <anthony.huazhang@gmail.com>
2017-12-01 20:28:04 -0500

    Added Student Grades.

---
+++ Student Grades/Student Grades/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Solution
+{
+    static int[] solve(int[] grades)
+    {
+        double[] toRound = Array.ConvertAll(grades, x => (double)x);
+        double temp;
+        for (int i = 0; i < grades.Length; i++)
+        {
+            if (toRound[i] > 37)
+            {
+                temp = Math.Round(toRound[i] / 5) * 5;
+                if(temp > toRound[i])
+                {
+                    toRound[i] = temp;
+                }
+            }
+        }
+        grades = Array.ConvertAll(toRound, x => (int)x);
+        return grades;
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] grades = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            grades[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int[] result = solve(grades);
+        Console.WriteLine(String.Join("\n", result));
+    }
+}
